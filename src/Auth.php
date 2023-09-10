1e64d2067b7b1e4ed91986d6a3b1c789d4518ec8
André Rosa <crosa7000@gmail.com>
2023-09-10 22:35:29 +0100

    FIX: Check session exists on expireSession

--- src/Auth.php
+++ src/Auth.php
@@ -487,6 +487,7 @@ class Auth extends Core
      */
     private static function expireSession(): bool
     {
+        self::sessionCheck();
         $sessionTtl = static::$session->get('SESSION_TTL');
 
         if (!$sessionTtl) {
