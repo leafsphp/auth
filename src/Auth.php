3e8e73347335db765ca4962a497365553d32da57
fadrian06 <franyeradriansanchez@gmail.com>
2025-11-05 20:31:23 -0400

    if !$this->user() is true, user is not authenticated, else php checks the right side of ||, for then $this->user() will be Leaf\Auth\User

--- src/Auth.php
+++ src/Auth.php
@@ -762,7 +762,7 @@ class Auth
 
         if ($middleware === 'is') {
             return app()->registerMiddleware('is', function ($role) use ($callback) {
-                if (!$this->user() || ($this->user() && $this->user()->isNot($role))) {
+                if (!$this->user() || $this->user()->isNot($role)) {
                     $callback($role);
                     exit;
                 }
@@ -771,7 +771,7 @@ class Auth
 
         if ($middleware === 'isNot') {
             return app()->registerMiddleware('isNot', function ($role) use ($callback) {
-                if (!$this->user() || ($this->user() && $this->user()->is($role))) {
+                if (!$this->user() || $this->user()->is($role)) {
                     $callback($role);
                     exit;
                 }
@@ -780,7 +780,7 @@ class Auth
 
         if ($middleware === 'can') {
             return app()->registerMiddleware('can', function ($role) use ($callback) {
-                if (!$this->user() || ($this->user() && $this->user()->cannot($role))) {
+                if (!$this->user() || $this->user()->cannot($role)) {
                     $callback($role);
                     exit;
                 }
@@ -789,7 +789,7 @@ class Auth
 
         if ($middleware === 'cannot') {
             return app()->registerMiddleware('cannot', function ($role) use ($callback) {
-                if (!$this->user() || ($this->user() && $this->user()->can($role))) {
+                if (!$this->user() || $this->user()->can($role)) {
                     $callback($role);
                     exit;
                 }
