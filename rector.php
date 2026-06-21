1e18ac1bbc943e51ad96c52438d04a6409a12e63
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-21 00:43:20 -0400

    require at least php 8.0, 'cause "firebase/php-jwt": "^7.0" also requires it

--- rector.php
+++ rector.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withDowngradeSets(false, false, false, false, false, true)
+    ->withDowngradeSets(php80: true)
     ->withFluentCallNewLine()
-    ->withImportNames(true, true, true, true)
+    ->withImportNames(
+        importNames: false,
+        importDocBlockNames: false,
+        importShortClasses: false,
+        removeUnusedImports: true,
+    )
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
